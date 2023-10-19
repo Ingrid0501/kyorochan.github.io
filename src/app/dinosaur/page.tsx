79d2b1626f71b352fa9ac898a307e1ec596cd587
Ingrid0501 <vari21s18@gmail.com>
2023-10-19 22:41:22 +0800

    'dinosaur'

---
+++ src/app/dinosaur/page.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+
+const Dinosaur=()=>{
+
+    return (
+
+    
+    <main>
+        <div>
+            This dinosaur figure comes from Britannica.
+        </div>
+        <div>
+            <Image src="https://cdn.britannica.com/50/8050-050-D930CD43/Mesozoic-Era-Age-of-Dinosaurs-periods-dinosaurs.jpg" alt="dinodino" />
+            </div>
+    </main>
+
+    )
+
+}
+
+export default Dinosaur
+
